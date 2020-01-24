49396288b7f56b2c4dcfa77ae6812b461575cbb1
Yulian Ovdey <yulian.ovdey@perch.com>
2020-01-24 18:02:26 -0500

    try updating tabs for single recipient

--- index.js
+++ index.js
@@ -64,16 +64,36 @@ const request = require('request-promise-native').defaults({
     });
   }
 
+  async function updateRecipientTabs(envelopeId, recipientId, tabs) {
+    return request({
+      method: 'PUT',
+      uri: `/envelopes/${envelopeId}/recipients/${recipientId}/tabs`,
+      body: tabs
+    });
+  }
 
   const envelopeId = await createEnvelopeFromTemplate();
   const { envelopeDocuments } = await listEnvelopeDocuments(envelopeId);
   const { documentId } = envelopeDocuments[0];
+
   const tabs = await getDocumentTabs(envelopeId, documentId);
 
+  const date = new Date().getTime().toString();
+
+  tabs['textTabs'][0].value = date;
+
+  // Updating all the tabs at once doesn't work
   try {
     await updateDocumentTabs(envelopeId, documentId, tabs);
     console.log('Success!');
   } catch (e) {
     console.log(e.message);
   }
+
+  // Updating the tabs for a single recipient does work
+  await updateRecipientTabs(envelopeId, tabs['textTabs'][0]['recipientId'], tabs);
+
+  const newTabs = await getDocumentTabs(envelopeId, documentId);
+
+  console.log(newTabs['textTabs'][0]['value'] === date);
 })();
